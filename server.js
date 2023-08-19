e4cf7b9c861ab12b8085d466be9ce2809d08f4df
Brian Kimbrow <bkimbrow25@gmail.com>
2023-08-19 14:48:35 -0500

    added bookHandler

--- server.js
+++ server.js
@@ -11,16 +11,6 @@ const mongoDB = process.env.DATABASE_URL;
 app.use(cors());
 
 // add the start up here
-<<<<<<< HEAD
-// This code block came from mongoose documentation 
-mongoose.set("strictQuery", false);
-
-// main().catch((err) => console.log(err));
-// async function main() {
-//   await mongoose.connect(mongoDB);
-//   console.log("MongoDb Connected")
-// }
-=======
 // This codeblock came from mongoose documentation
 mongoose.set("strictQuery", false);
 
@@ -29,7 +19,6 @@ async function main() {
   await mongoose.connect(mongoDB);
   console.log("MongoDb Connected");
 }
->>>>>>> main
 
 const Book = require("./Book");
 
@@ -37,9 +26,6 @@ app.get("/test", (request, response) => {
   response.send("Hello World");
 });
 
-<<<<<<< HEAD
-app.get("/books", bookHandler)
-=======
 app.get("/books", async (request, response) => {
   try {
     const books = await Book.find();
@@ -48,7 +34,6 @@ app.get("/books", async (request, response) => {
     response.status(500).json({ error: "Error to Server" });
   }
 });
->>>>>>> main
 
 app.listen(3001, () => {
   console.log("Listen on the port 3001...");
